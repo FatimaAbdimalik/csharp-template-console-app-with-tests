dcbb5f49f1f29770bfb2f2271e5e7fa7100c70de
Neil Hughes <neil.hughes@techreturners.com>
2022-05-20 10:27:33 +0100

    Initial commit

---
+++ Code/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TemplateConsoleApp
+{
+    public class Solution
+    {      
+        // rename this method, alter the return type and params if necessary
+        public void Solve(/* params */)
+        {
+            // code here                        
+        }
+
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using System.Collections.Generic;
+using System;
+using TemplateConsoleApp;
+
+namespace TemplateConsoleApp.Tests
+{
+    public class Tests
+    {
+        private Solution solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            solution = new Solution();
+        }
+
+        [Test]
+        // Replace this test with the first real test
+        public void DummyTest()
+        {
+            Assert.AreEqual(true, true);
+            Assert.AreNotEqual(true, false);
+        }
+       
+
+        
+       
+    }
+}
